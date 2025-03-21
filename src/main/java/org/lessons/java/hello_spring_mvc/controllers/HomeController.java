a70999b0444940a45febacf4efc3522ba54ec3ed
SimoneChiodo <simochi.005@gmail.com>
2025-03-21 18:36:47 +0100

    add details.html, info.html, HomeController routes

--- src/main/java/org/lessons/java/hello_spring_mvc/controllers/HomeController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controllers/HomeController.java
@@ -1,20 +1,29 @@
 package org.lessons.java.hello_spring_mvc.controllers;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-  
-  public String start(){
-    return "Ciao!";
-  }
-
   @GetMapping("/info")
   public String info(){
-    return "Informazioni!";
+    return "info";
   }
+
+  @GetMapping("/details/{id}")
+  public String productDetails(@PathVariable("id") String id, Model model) {
+      model.addAttribute("productId", id);
+      model.addAttribute("date", LocalDateTime.now());
+
+      return "details";
+  }
+  
 
 }
